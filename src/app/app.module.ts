808eab3225a8c6a382b4aa664a31ebd40dc5b5ec
tajleena <tajleena15@gmail.com>
2020-02-03 12:03:48 +0530

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MobileComponent } from './mobile/mobile.component';
 import { BookComponent } from './book/book.component';
+import { BlueComponent } from './blue/blue.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,ReactiveFormsModule ],
-  declarations: [ AppComponent, HelloComponent, MobileComponent, BookComponent ],
+  declarations: [ AppComponent, HelloComponent, MobileComponent, BookComponent, BlueComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
